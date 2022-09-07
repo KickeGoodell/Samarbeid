30f1428545b55cf0050759d869002bfa6aa93dfb
JWoodh <jowoa002@osloskolen.no>
2022-09-07 13:19:01 +0200

    Hello again!

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        String helloWorld[] = {"H", "e", "l", "l", "o"};
+        for(int i = 0; i < helloWorld.length; i++) {
+            String str = helloWorld[i];
+            System.out.println(str);
+        }
+    }
+}
